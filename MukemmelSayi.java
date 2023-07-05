270bbdf06041b41773160bbc0cbbf68d8eabd25d
elifinurv <138496541+elifinurv@users.noreply.github.com>
2023-07-05 09:08:17 +0300

    Update MukemmelSayi.java

--- MukemmelSayi.java
+++ MukemmelSayi.java
@@ -1,22 +1,16 @@
 
 import java.util.Scanner;
 
-/**
- *
- * @author Eren Çetinkaya Cruz
- */
 public class MukemmelSayi {
 
  public static void main(String[] args) {
-  // TODO Auto-generated method stub
-
+ //mükemmel sayi
   Scanner cr = new Scanner(System.in);
   
   int sayi;
   System.out.println("Bir sayi giriniz : ");
   sayi = cr.nextInt();  
   
-  //kontrol edilecek sayıyı fonksiyonumuza yolluyoruz 
   if(mukemmelSayiBul(sayi)){
    System.out.println(sayi+" sayısı mükemmel sayıdır.");
   }else{
@@ -28,14 +22,12 @@ public class MukemmelSayi {
  
  public static boolean mukemmelSayiBul(int sayi) {
   
-  // kendisinden küçük tüm sayılara bölüyoruz ve tam bölen sayıları topluyoruz
   int toplam = 0;
   for (int i = 1; i < sayi; i++) {
    if (sayi % i == 0) {
     toplam += i;
    }
   }
-  //toplam, sayımıza eşit ise mükemmel sayı olmuş oluyor 
   if (toplam == sayi) {
    return true;
   } else {
