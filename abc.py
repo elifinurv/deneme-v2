0371a95c89fe1e2689e8a4a4388543dd09b267cf
elifinurv <138496541+elifinurv@users.noreply.github.com>
2023-11-22 14:11:36 +0300

    Create abc.py

---
+++ abc.py
@@ -0,0 +1,17 @@
+def ArtıkYıl(yıl):
+    artık=False
+    if yıl%400==0 or (yıl%4==0 and yıl%100!=0): artık=True
+    return artık
+ 
+def YılınGünü(Ay,Gün,Yıl):
+    günler=[31,28,31,30,31,30,31,31,30,31,30,31]
+    if ArtıkYıl(Yıl):
+        günler[1]=29
+    sıra=0
+    for a in range(Ay-1):
+        sıra+=günler[a]
+    sıra+=Gün
+    return sıra
+ 
+print(YılınGünü(4,9,2018))
+ 
